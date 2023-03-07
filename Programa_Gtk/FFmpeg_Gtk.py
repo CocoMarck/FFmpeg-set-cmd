15676ef503f67c20610c302d5865737d49de8437
CocoMarck <76503881+CocoMarck@users.noreply.github.com>
2023-03-06 21:27:51 -0600

    Add files via upload

--- Programa_Gtk/FFmpeg_Gtk.py
+++ Programa_Gtk/FFmpeg_Gtk.py
@@ -34,10 +34,44 @@ class Dialog_Start(Gtk.Dialog):
         print('Boton de prueba, precionado')
 
 
+class Command_Run(Gtk.Dialog):
+    def __init__(self, parent, cfg='', txt='Ejecutar Comando'):
+        super().__init__(title='Ejecutar comando', transient_for=parent, flags=0)
+        self.set_default_size(512, 0)
+        self.cfg = cfg
+        
+        box_v = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
+        
+        label = Gtk.Label()
+        label.set_markup(f'<b>Comando</b>')
+        label.set_justify(Gtk.Justification.CENTER)
+        label.set_line_wrap(True)
+        box_v.pack_start(label, True, True, 0)
+        
+        label = Gtk.Label()
+        label.set_markup(f'<i>{self.cfg}</i>')
+        label.set_justify(Gtk.Justification.LEFT)
+        label.set_line_wrap(True)
+        label.set_selectable(True)
+        box_v.pack_start(label, True, True, 0)
+        
+        button = Gtk.Button(label=txt)
+        button.connect('clicked', self.evt_command_run)
+        box_v.pack_start(button, True, True, 0)
+        
+        box_main = self.get_content_area()
+        box_main.add(box_v)
+        self.show_all()
+        
+    def evt_command_run(self, widget):
+        os.system(f'xfce4-terminal --startup-id= -e "{self.cfg}"')
+        self.destroy()
+
+
 class Dialog_FFmpegVideo(Gtk.Dialog):
     def __init__(self, parent, opc='CompressVideos'):
-        if opc == 'VideoCompress': txt_title = 'Comprimir Videos'
-        elif opc == 'VideoRecord': txt_title = 'Grabar Videos'
+        if opc == 'VideoCompress': txt_title = 'Comprimir Video'
+        elif opc == 'VideoRecord': txt_title = 'Grabar Video'
         else: 'Title for else'
     
         super().__init__(title=f'{opc}', transient_for=parent, flags=0)
@@ -49,18 +83,22 @@ class Dialog_FFmpegVideo(Gtk.Dialog):
         label_title.set_markup(f'<b>{txt_title}</b>\n')
         box_data.pack_start(label_title, True, True, 8)
         
-        btn_path = Gtk.Button(label='Elegir Video')
-        btn_path.connect("clicked", self.evt_path)
+        if opc == 'VideoCompress':
+            btn_path = Gtk.Button(label='Elegir Video')
+            btn_path.connect("clicked", self.evt_path)
+            box_data.pack_start(btn_path, True, True, 0)
+        elif opc == 'VideoRecord':
+            print('Seleccionar carpeta y escribir nombre de video')
+        else: pass
         self.pth = ''
-        box_data.pack_start(btn_path, True, True, 0)
 
 
         crf_box = Gtk.Box(spacing=4)
         box_data.pack_start(crf_box, True, True, 0)
 
-        crf_label = Gtk.Label(label='Calidad (CRF de 0-50)')
-        crf_label.set_justify(Gtk.Justification.LEFT)
-        crf_box.pack_start(crf_label, False, False, 0)
+        self.crf_CheckButton = Gtk.CheckButton(label='Calidad (CRF de 0-50)')
+        self.crf_CheckButton.set_active(True)
+        crf_box.pack_start(self.crf_CheckButton, False, False, 0)
 
         crf_SpinButton_adj = Gtk.Adjustment(
                                 upper=50, step_increment=1, page_increment=10, 
@@ -69,15 +107,15 @@ class Dialog_FFmpegVideo(Gtk.Dialog):
         self.crf_SpinButton = Gtk.SpinButton()
         self.crf_SpinButton.set_adjustment(crf_SpinButton_adj)
         self.crf_SpinButton.set_numeric(True)
-        crf_box.pack_start(self.crf_SpinButton, True, True, 0)
+        crf_box.pack_start(self.crf_SpinButton, False, False, 8)
         
 
         fps_box = Gtk.Box(spacing=4)
         box_data.pack_start(fps_box, True, True, 0)
         
-        fps_label = Gtk.Label(label='Fotogramas (FPS)')
-        fps_label.set_justify(Gtk.Justification.LEFT)
-        fps_box.pack_start(fps_label, False, False, 0)
+        self.fps_CheckButton = Gtk.CheckButton(label='Fotogramas (FPS)')
+        self.fps_CheckButton.set_active(True)
+        fps_box.pack_start(self.fps_CheckButton, False, False, 0)
         
         self.fps_SpinButton = Gtk.SpinButton()
         fps_SpinButton_adj = Gtk.Adjustment(
@@ -87,33 +125,35 @@ class Dialog_FFmpegVideo(Gtk.Dialog):
         self.fps_SpinButton.set_range(1, 100)
         self.fps_SpinButton.set_value(25)
         self.fps_SpinButton.set_numeric(True)
-        fps_box.pack_start(self.fps_SpinButton, True, True, 0)
+        fps_box.pack_start(self.fps_SpinButton, False, False, 34)
         
         
         rez_box = Gtk.Box(spacing=4)
         box_data.pack_start(rez_box, True, True, 0)
         
-        rez_label = Gtk.Label(label='Resolucion (H x V)')
-        rez_label.set_justify(Gtk.Justification.LEFT)
-        rez_box.pack_start(rez_label, False, False, 0)
+        self.rez_CheckButton = Gtk.CheckButton(label='Resolucion (H x V)')
+        self.rez_CheckButton.set_active(True)
+        rez_box.pack_start(self.rez_CheckButton, False, False, 0)
         
         self.rez_entryH = Gtk.Entry()
         self.rez_entryH.set_text('1280')
-        rez_box.pack_start(self.rez_entryH, True, True, 0)
+        self.rez_entryH.set_width_chars(8)
+        rez_box.pack_start(self.rez_entryH, False, False, 0)
         
         rez_label_HxV = Gtk.Label(label='x')
-        rez_box.pack_start(rez_label_HxV, True, True, 0)
+        rez_box.pack_start(rez_label_HxV, False, False, 0)
         
         self.rez_entryV = Gtk.Entry()
         self.rez_entryV.set_text('720')
-        rez_box.pack_start(self.rez_entryV, True, True, 0)
+        self.rez_entryV.set_width_chars(8)
+        rez_box.pack_start(self.rez_entryV, False, False, 0)
         
         
         self.label_path = Gtk.Label(label='(Aqui se mostrara el Video)')
         self.label_path.set_line_wrap(True)
         box_data.pack_start(self.label_path, True, True, 8)
         
-        btn_add_cfg = Gtk.Button(label='Aceptar')
+        btn_add_cfg = Gtk.Button(label=txt_title)
         btn_add_cfg.connect('clicked', self.evt_add_cfg)
         box_data.pack_start(btn_add_cfg, True, True, 0)
         
@@ -126,14 +166,20 @@ class Dialog_FFmpegVideo(Gtk.Dialog):
         
         if self.pth == '': print('No se a seleccionado el Video')
         else:
-            crf = FFmpeg.CRF(self.crf_SpinButton.get_value_as_int())
+            if self.crf_CheckButton.get_active() == True:
+                crf = FFmpeg.CRF(self.crf_SpinButton.get_value_as_int())
+            else: crf = ''
             
-            fps = FFmpeg.FPS(self.fps_SpinButton.get_value_as_int())
-
-            rez_HxV = FFmpeg.Resolution(
-                rez_H=self.rez_entryH.get_text(), 
-                rez_V=self.rez_entryV.get_text()
-            )
+            if self.fps_CheckButton.get_active() == True:
+                fps = FFmpeg.FPS(self.fps_SpinButton.get_value_as_int())
+            else: fps = ''
+            
+            if self.rez_CheckButton.get_active() == True:
+                rez_HxV = FFmpeg.Resolution(
+                    rez_H=self.rez_entryH.get_text(), 
+                    rez_V=self.rez_entryV.get_text()
+                )
+            else: rez_HxV = ''
 
             print(
                 f'Video seleccionado: "{self.pth}\n"'
@@ -141,9 +187,13 @@ class Dialog_FFmpegVideo(Gtk.Dialog):
                 f'Los FPS seran "{fps}"\n'
                 f'La resolución sera "{rez_HxV}"'
             )
-            os.system('xfce4-terminal --startup-id= -e '
-                f"""'ffmpeg -i "{self.pth}" {crf} {fps} {rez_HxV} "{self.pth}_Comprimido.mkv"'"""
+            
+            cfg = (f'ffmpeg -i "{self.pth}" {crf} {fps} {rez_HxV} '
+                f'"{self.pth}_Comprimido.mkv"'
             )
+            dialog = Command_Run(self, cfg=cfg, txt='Comprimir Video')
+            rsp = dialog.run()
+            dialog.destroy()
         
             
     def evt_path(self, widget):
